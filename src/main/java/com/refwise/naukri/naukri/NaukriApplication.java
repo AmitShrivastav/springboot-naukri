73886298daf4e767914635963514b75f4a6afb87
AmitShrivastav <amit.mit.shrivastava@gmil.com>
2021-08-01 17:36:01 +0530

    Add service dicovery client

--- src/main/java/com/refwise/naukri/naukri/NaukriApplication.java
+++ src/main/java/com/refwise/naukri/naukri/NaukriApplication.java
@@ -4,12 +4,12 @@ package com.refwise.naukri.naukri;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 
 @SpringBootApplication
-//@EnableAutoConfiguration
-//@EnableAspectJAutoProxy
+@EnableDiscoveryClient
 public class NaukriApplication {
 
 	public static void main(String[] args) {
